1337f9a66d9b5aef98ea55a01bca1b04891c7142
wil <elsalvadorazul@outlook.es>
2022-12-09 15:48:49 -0600

    captura de codigo inicial y todo esta bien

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::view('/', 'welcome');
+Route::view('/contacto', 'contact');
+Route::view('/blog', 'blog');
+Route::view('/about', 'about');
+    
